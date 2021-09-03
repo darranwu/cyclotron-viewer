dfb015370d4f6d4d17cb1de4b37e9b63f30c8786
Felix Lawrence <felix.lawrence@gmx.com>
2021-09-02 21:32:05 -0700

    Found code

---
+++ js/viewer.js
@@ -0,0 +1,141 @@
+if (webrtcDetectedBrowser != 'chrome') {
+    var constraints = {audio: false, video: { facingMode: 'environment' }};
+    attachStream(constraints);
+} else {
+    var chromeVideoDeviceInfos;
+    var chromeVideoDeviceIndex = 0;
+    getSourcesForChrome();
+}
+
+function attachStream(constraints) {
+    if (window.stream) {
+        // Required for Chrome
+        window.stream.getTracks().forEach(track => track.stop());
+    }
+    navigator.mediaDevices.getUserMedia(constraints)
+    .then(function(stream) {
+        window.stream = stream;
+        video.srcObject = stream;
+        document.getElementById('entertainThePeasants').style.display = 'none'
+        document.getElementById('viewer').style.display = 'block'
+    })
+    .catch(err => console.error(err));
+}
+
+var canvas = document.getElementById('display'),
+    ctx = canvas.getContext('2d');
+
+video.addEventListener('resize', function(e) {
+    console.log('video resized');
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
+});
+
+var lastDimmed = 0,
+    msPerDim = 25;
+
+video.addEventListener('playing', blendWithLast, false);
+function blendWithLast() {
+
+    var now = performance.now();
+
+    if (lastDimmed == 0) {
+        lastDimmed = now;  // Initialisation
+    }
+    var ptsToDim = Math.floor((now - lastDimmed) / msPerDim);
+
+    if (ptsToDim > 3) {
+        ptsToDim = Math.min(ptsToDim, 255);
+        // dim old pixels slightly by overlaying slightly-transparent layer 
+        ctx.fillStyle = 'RGBA(0,0,0,' + ptsToDim/255 + ')';
+        ctx.globalCompositeOperation = 'source-over';
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+        lastDimmed += ptsToDim * msPerDim;
+    }
+
+    // pix = max(new_pix, old_pix (maybe dimmed))
+    ctx.globalCompositeOperation = 'lighten'
+
+    // Get a new frame
+    ctx.drawImage(video, 0, 0);
+
+    requestAnimationFrame(blendWithLast);
+}
+
+takePhotoButton.addEventListener('click', takePhoto);
+function takePhoto() {
+    ctx.globalCompositeOperation = 'source-over'
+    var dWidth = Math.max(canvas.width, canvas.height) * 0.17,
+        dHeight = dWidth / logoWatermark.width * logoWatermark.height,
+        dx = canvas.width * 0.98 - dWidth,
+        dy = canvas.height * 0.98 - dHeight;
+    ctx.drawImage(logoWatermark, dx, dy, dWidth, dHeight);
+    download('cyclotron.png', canvas.toDataURL('image/png'));
+}
+takePhotoButton.addEventListener('mousedown', function(){
+    takePhotoButton.firstElementChild.src = "img/shutter-pressed.png";
+});
+takePhotoButton.addEventListener('touchstart', function(){
+    takePhotoButton.firstElementChild.src = "img/shutter-pressed.png";
+});
+window.addEventListener('mouseup', function(){
+    takePhotoButton.firstElementChild.src = "img/shutter-button.png";
+});
+window.addEventListener('touchend', function(){
+    takePhotoButton.firstElementChild.src = "img/shutter-button.png";
+});
+
+// recordVideoButton.addEventListener('click', recordVideo);
+// function recordVideo() {
+//     var options = {
+//        type: 'canvas'
+//     };
+//     var recordRTC = RecordRTC(canvas, options);
+//     recordRTC.setRecordingDuration(
+//         5 * 1000, gifURL => recordRTC.save('bla.webm')
+//     );
+//     recordRTC.startRecording();
+// }
+
+function download(filename, dataURL) {
+    var link = document.createElement('a');
+    link.download = filename;
+    link.href = dataURL;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
+
+function getSourcesForChrome() {
+    navigator.mediaDevices.enumerateDevices()
+        .then(gotDevicesForChrome)
+        .catch(err => console.error(err));
+}
+
+function gotDevicesForChrome(deviceInfos) {
+    console.log(deviceInfos);
+    chromeVideoDeviceInfos = deviceInfos.filter(
+        (el, i, arr) => (el.kind === 'videoinput') 
+    );
+    if (chromeVideoDeviceInfos.length > 1) {
+        showCameraToggleButtonForChrome();
+    }
+    var constraints = {audio: false, video: { deviceId: chromeVideoDeviceInfos[chromeVideoDeviceIndex].deviceId }};
+    attachStream(constraints);
+}
+
+function changeDeviceForChrome() {
+    chromeVideoDeviceIndex = (chromeVideoDeviceIndex + 1) % chromeVideoDeviceInfos.length;
+    var constraints = {audio: false, video: { deviceId: chromeVideoDeviceInfos[chromeVideoDeviceIndex].deviceId }};
+    attachStream(constraints);
+}
+
+function showCameraToggleButtonForChrome() {
+    var button = document.getElementById('cameraToggleForChrome');
+    cameraToggleForChrome.style.display = 'inline';
+    // recordVideoButton.style.display = 'inline';
+    cameraToggleForChrome.addEventListener('click', function(ev){
+        changeDeviceForChrome();
+        ev.preventDefault();
+    }, false);
+}
